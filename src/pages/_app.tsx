036150d9aaedb948d17691688c247912718d9eb3
Daniela Vornic <daniela.vornic@gmail.com>
2024-05-18 21:17:20 +0300

    Add inter and fix lib folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="light">
-        <Component {...pageProps} />
+        <div className={inter.className}>
+          <Component {...pageProps} />
+        </div>
       </NextThemesProvider>
     </NextUIProvider>
   );
