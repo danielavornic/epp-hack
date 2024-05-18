33e51bb26b500923e999eda46c2061f6ce39ad39
Daniela Vornic <daniela.vornic@gmail.com>
2024-05-18 20:57:05 +0300

    Add initial packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
---
+++ src/pages/lib/axios.ts
@@ -0,0 +1,7 @@
+import $axios from "axios";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const axios = $axios.create({
+  baseURL: baseUrl
+});
